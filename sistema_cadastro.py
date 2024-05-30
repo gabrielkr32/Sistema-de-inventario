0bb1d175cdaea1b967ce8f484b68ee8b183b824a
Gabriel Kruger <gabrielkr32@gmail.com>
2024-05-30 20:49:36 -0300

    inventario.csv

--- sistema_cadastro.py
+++ sistema_cadastro.py
@@ -2,19 +2,47 @@
 #Detalhes: nome- numero de sereal- data de entrada/saida- setor
 
 
-inventario = [] 
-nome = input("Nome do Equipamento: ") # pede o nome do item
-inventario.append(nome) #adicona o nome da lista
-print(inventario)
+inventario = {}
+while True:
+     opcao = int(input("""Digite:
+                       1 para adicinar
+                       2 persistir no arquivo
+                       3 para exibir maquina
+                       4 para excluir
+                       5 para sair"""))
+     if opcao == 1:
+          resposta = "S"
+          while resposta =="S":
+               numero_sereal = input("SEREAL: ")
+               data_Entrada = input("Digite a data de entrada: ")
+               nome = input("Nome do equipamento: ")
+               departamento = input("DEPARTAMENTO: ")
 
-numero_serie = int(input("Numero de sereal: ")) #entrada de numero
-inventario.append(numero_serie) #adiciona o numero sereal
-print(inventario)
+               inventario[numero_sereal] = [data_Entrada, nome, departamento]
+               resposta = input("DIgite 'S' para continuar: ").upper()
 
-data_entrada = (input("Data de entrada: ")) #Data de entrada do objeto
-inventario.append(data_entrada) # // data
-print(inventario)
 
-setor = input("Setor de destino: ")#Nome do setor
-inventario.append(setor)# // setor
-print(inventario)
+     elif opcao == 2:
+          with open("Inventario.csv", "a") as inv:
+               for chave, valor, in inventario.items():
+                    inv.write(chave + ";" + valor [0] + ";" +
+                              valor [1] + ":" + valor [2] + "\n")
+                    print("Persistindo com sucesso")
+
+
+     elif opcao == 3:
+          with open("Inventario.cvs", "r") as inv:
+               print(inv.readlines()) 
+
+
+     #elif opcao == 4:
+         # os.remove() inventario[numero_sereal, data_Entrada, nome, departamento]
+          #print("** Arquivo deletado com sucesso **")
+
+
+     elif opcao == 5:
+          break
+     else:
+          print("** Opcao invalida **")     
+   #erros na linha 39
+   #erros para adicionar no inventario 
